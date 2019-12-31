b8fa53fbd6b175f9906e9ec90603276617058450
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-12-30 22:12:42 -0300

    Ajuste visual no Drawer e List;

--- src/components/Layout/Sidebar/Drawer/styled/index.js
+++ src/components/Layout/Sidebar/Drawer/styled/index.js
@@ -5,15 +5,13 @@ export const StyledRoot = styled.div`
     display: flex;
     flex-direction: column;
     align-items: space-between;
-    box-shadow: 4px 0px 8px -8px rgba(0,0,0,0.75);!important;
-    height: 100vh;
+    box-shadow: 4px 0px 8px -9px rgba(0,0,0,0.75);!important;
     transition: all .60s ease;
-
     ${props => props.openDrawer ? 
     `
         position: relative;
         width: 280px;
-        border-right: none;
+
     ` : `
         overflow-x: hidden;
         width: 60px;
--- src/components/Layout/Sidebar/List/styled/index.js
+++ src/components/Layout/Sidebar/List/styled/index.js
@@ -5,6 +5,4 @@ export const StyledRoot = styled.div`
     display: flex;
     flex-direction: column;
     align-items: space-between;
-    box-shadow: 4px 0px 8px -8px rgba(0,0,0,0.75);!important;
-    height: 100%;
 `;
